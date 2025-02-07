359e7b3e8d16844eaaac2a1a12ee946391cdb547
guruhikify <167468077+guruhikify@users.noreply.github.com>
2025-02-07 17:49:03 +0530

    Add files via upload

---
+++ justdial.stories.jsx
@@ -0,0 +1,8 @@
+import JustDial from './justdial'; // Import the component
+
+export default {
+  title: 'Components/JustDial', // Storybook title
+  component: JustDial,
+};
+
+export const Default = () => <JustDial />;
