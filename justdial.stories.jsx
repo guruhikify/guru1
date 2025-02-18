a9e038514d1169056bf2155165730af231588b20
guruhikify <167468077+guruhikify@users.noreply.github.com>
2025-02-18 12:37:30 +0530

    Delete justdial.stories.jsx

--- justdial.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import justdial from "../justdial";
-export default{
-    title: 'Components/judtdial/justdial',
-    component: justdial,
-  };
-  
-
-export const Default=()=><justdial/>
